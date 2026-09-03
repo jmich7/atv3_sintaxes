6227d09934f611c7a5a8b52083a530b8982b6c7b
Joao <jgmichelan1@gmail.com>
2026-09-02 21:50:29 -0400

    Adiciona atividade 2 - calculo de IMC

---
+++ atividade2/imc.py
@@ -0,0 +1,11 @@
+peso = float(input("Digite seu peso: "))
+altura = float(input("Digite sua altura: "))
+imc = peso / altura ** 2
+if imc < 18.5:
+    print("Abaixo do peso")
+elif imc < 25:
+    print("Peso normal")
+elif imc < 30:
+    print("Sobrepeso")
+else:
+    print("Obesidade")
